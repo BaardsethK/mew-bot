e79cfb3395f8da8826a488bd42bd9b4f4cc3944f
baardsethk <krisbaa@stud.ntnu.no>
2019-07-17 17:31:49 +0200

    Added pickling for mood - storage based on server ID

--- mew-bot.py
+++ mew-bot.py
@@ -13,16 +13,24 @@ import praw
 import time
 import random
 import csv
-
+import pickle
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 TOKEN = os.getenv('DISCORD_TOKEN')
-BOT_PREFIX = ('!m ', '!mew ', '!MEW ', '!M ')
+BOT_PREFIX = ('!m ', '!mew ', '!Mew ', '!MEW ', '!M ')
 
 description='''Mew - I do stuff!'''
 bot = commands.Bot(command_prefix = BOT_PREFIX, description=description)
+
+
+try:
+    mood = pickle.load(open("mood.pickle", "rb"))
+except (OSError, IOError) as e:
+    mood = {"":"formal"}
+    pickle.dump(mood, open("mood.pickle", "wb"))
+    
 
 @bot.command(name='test', description='Test for bot.command-functionality', aliases=['TEST'], pass_context=True)
 async def test_command(context):
@@ -41,24 +49,31 @@ async def bfv_stats(context, origin_alias):
     msg = comut.get_bfv_stats(origin_alias)
     await context.send(msg)
 
-@bot.command(name='uwuize', pass_context=True)
+@bot.command(name='uwuize', aliases=['uwu'], pass_context=True)
 async def uwuize(context, *, message):
     msg = message.lower()
+    msg = msg.replace('pos', 'paws')
     msg = msg.replace('r', 'w')
     msg = msg.replace('l', 'w')
     msg = msg.replace('th', 'd')
     await context.send(msg)
     
+@bot.command(name='moodify', aliases=['mood'], pass_context=True)
+async def save_mood(context, mood):
+    server_id = context.message.server.id
+    mood[server_id] = mood
+    pickle.dump(mood, open("mood.pickle", "wb"))
     
 @bot.event
 async def on_message(message):
     if message.author.bot == False:
         channel = message.channel
+        server_id = message.guild.id
         if '/r/' in message.content:
             subreddit = re.search(r'\/r\/((.*?)[^\s]+|[^\/]+)', message.content)
             print(subreddit.group(0))
             if len(subreddit.group(1)) > 20:
-                msg = '''Silly user, that's not a subreddit.'''
+                msg = comut.get_message_line('dismissive', mood[server_id])
                 await channel.send(msg)
                 await bot.process_commands(message)
             elif subreddit:
@@ -66,11 +81,11 @@ async def on_message(message):
                 await channel.send(msg)
                 await bot.process_commands(message)
         elif 'bad bot' in message.content.lower():
-            msg = comut.get_message_line('sad', 'formal')
+            msg = comut.get_message_line('sad', mood[server_id])
             await channel.send(msg)
             await bot.process_commands(message)
         elif 'good bot' in message.content.lower():
-            msg = comut.get_message_line('happy', 'formal')
+            msg = comut.get_message_line('happy', mood[server_id])
             await channel.send(msg)
             await bot.process_commands(message)
         else:
