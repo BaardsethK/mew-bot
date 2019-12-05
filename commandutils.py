7c5809a018b21c248591aab8b5abe5320e4e945a
BaardsethK <kristobaar@hotmail.com>
2019-12-05 21:27:26 +0100

    Reformatted entire code

--- commandutils.py
+++ commandutils.py
@@ -3,108 +3,94 @@ import json
 import re
 import csv
 import os
-from PIL import Image, ImageSequence, GifImagePlugin
+
+from PIL import Image
+from PIL import ImageSequenece
+from PIL import GifImagePlugin
+
 import ffmpeg
 
+GAME_STAT_PAGES = {
+    'bfv': 'https://api.tracker.gg/api/v1/bfv/standard/profile/origin/{}',
+    'apex': 'https://api.dreamteam.gg/games/apex/pc/players/{}/statistics',
+}
 
+GAME_STAT_PAGE_FORMAT = {
+    'bfv': '''Stats for origin alias: {}
+              Score = {}
+              Score/min: {}
+              K/D: {}
+              Kills: {}
+              Deaths: {}
+              Accuracy: {}
+              Headshots: {}
+              Longest headshot: {}
+              playtime: {}''',
+    'apex': '''Stats for origin alias: {}
+               Rank: {} {}
+               Rank points: {}/{}
+               Kills as {}: {} kills''',
+}
 
-def get_bfv_stats(origin_alias):
-        response = requests.get('https://api.tracker.gg/api/v1/bfv/standard/profile/origin/{}'.format(origin_alias))
-        try:
-            stats = response.json().get('data').get('stats')
-            spm = round(float(stats[0].get('value')), 2)
-            kd = round(float(stats[1].get('value')), 2)
-            kills = stats[2].get('value')
-            deaths = stats[3].get('value')
-            accr = round(float(stats[10].get('value')), 2)
-            heads = stats[15].get('value')
-            longhead = stats[17].get('value')
-            playtime = stats[37].get('displayValue')
-            score = stats[38].get('value')
-            msg = '''Stats for origin alias: {}
-            Score = {}
-            Score/min: {}
-            K/D: {}
-            Kills: {}
-            Deaths: {}
-            Accuracy: {}
-            Headshots: {}
-            Longest headshot: {}
-            playtime: {}'''.format(origin_alias, score, spm, kd, kills, deaths, accr, heads, longhead, playtime)
+def get_game_stats(game, alias):
+    if GAME_STAT_PAGES[game]:
+        url = GAME_STAT_PAGES[game]
+        response = requests.get(url.format(alias))
+        if game == 'bfv':
+            try:
+                stats = response.json().get('data').get('stats')
+                spm = round(float(stats[0].get('value')), 2)
+                kd = round(float(stats[1].get('value')), 2)
+                kills = stats[2].get('value')
+                deaths = stats[3].get('value')
+                accr = round(float(stats[10].get('value')), 2)
+                heads = stats[15].get('value')
+                longhead = stats[17].get('value')
+                playtime = stats[37].get('displayValue')
+                score = stats[38].get('value')
+
+                msg = GAME_STAT_PAGE_FORMAT[game].format(alias, 
+                                                         score, 
+                                                         spm,
+                                                         kd,
+                                                         kills, 
+                                                         deaths, 
+                                                         accr, 
+                                                         heads, 
+                                                         longhead, 
+                                                         playtime)
+                return msg
+        elif game == 'apex':
+            try:
+                stats = response.json()
+                rank = stats.get('player').get('rank').get('tier')
+                div = stats.get('player').get('rank').get('division')
+                legend = stats.get('statistics')[0].get('legend')
+                kills = stats.get('statistics')[0].get('kills')
+                level = stats.get('statistics')[1].get('level')
+                rank_points = stats.get('statistics')[1].get('rank_points')
+                next_rank_points = stats
+                                   .get('player')
+                                   .get('rank')
+                                   .get('next_rank_points')
+
+                msg = GAME_STAT_PAGE_FORMAT[game].format(alias, 
+                                                         rank, 
+                                                         div, 
+                                                         rank_points, 
+                                                         next_rank_points, 
+                                                         legend, 
+                                                         kills)
+                return msg
+        except:
+            print(response)
+            msg = "Failed to find that player. Are you sure you spelled the alias correctly?"
             return msg
-        except:                                                                                                                                                                                                
-            msg = "Failed to find that player. Are you sure you spelled it correctly?"
-            return msg
-           
-def get_apex_stats(origin_alias):
-    response = requests.get('https://api.dreamteam.gg/games/apex/pc/players/{}/statistics'.format(origin_alias))
-    try:
-        stats = response.json()
-        rank = stats.get('player').get('rank').get('tier')
-        div = stats.get('player').get('rank').get('division')
-        legend = stats.get('statistics')[0].get('legend')
-        kills = stats.get('statistics')[0].get('kills')
-        level = stats.get('statistics')[1].get('level')
-        rank_points = stats.get('statistics')[1].get('rank_points')
-        next_rank_points = stats.get('player').get('rank').get('next_rank_points')
-
-        msg = '''Stats for origin alias: {}
-        Rank: {} {}
-        Rank points: {}/{}
-        Kills as {}: {} kills
-        '''.format(origin_alias, rank, div, rank_points, next_rank_points, legend, kills)
-        return msg
-    except:
-        print(response)
-        msg = "Failed to find that player. Are you sure you spelled it correctly?"
-        return msg
-            
-def get_message_line(msg_type, mood_type):
-    msg = ''
-    with open('lines.json') as msg_file:
-        data = json.load(msg_file) 
-        data = data[msg_type]
-        msg_data = next((item for item in data if item["mood"] == mood_type))
-        msg = msg_data["msg"]
-        return msg
-
-def add_reaction_class(reaction_class):
-    reaction_class = reaction_class.lower()
-    with open('lines.json') as msg_file:
-        data = json.load(msg_file)
-        class_exists = reaction_class in data
-    if not class_exists:
-        with open('lines.json', 'w') as msg_file:
-            data[reaction_class] = ''
-            json.dump(data, msg_file)
-        return 'Message class "{}" created'.format(reaction_class)
     else:
-        return 'Message class "{}" already exists'.format(reaction_class)
+        msg = 'I cannot find statistics for that game'
+        return msg
 
-def add_reaction_message(reaction_class, reaction_mood, message):
-    reaction_class = reaction_class.lower()
-    reaction_mood = reaction_mood.lower()
-    with open('lines.json') as msg_file:
-        data = json.load(msg_file)
-        class_exists = reaction_class in data
-        try:
-            msg_class = data[reaction_class]
-            for item in msg_class:
-                if (item['mood'] == reaction_mood):
-                    mood_exists = True
-        except KeyError:
-            mood_exists = False
-            pass
-    if class_exists and not mood_exists:
-        with open('lines.json','w') as msg_file:
-            data[reaction_class].append({"mood": reaction_mood, "msg" :message})
-            json.dump(data,msg_file)
-            print('Added message {0}:{1} to class {2}'.format(reaction_mood, message, reaction_class))
-        return 'Added message reaction to file'
-    elif class_exists and mood_exists:
-        return 'This class mood and message already exists. You should make a pull request to change it, or ask the developer to get off his ass and do it himself'
-    elif not class_exists and not mood_exists:
-        return 'Class does not exist.'
+
 
 def combine_mpeg(first_url, second_url):
     endfile = './tmp/combined.gif'
@@ -117,7 +103,14 @@ def combine_mpeg(first_url, second_url):
 
     info = ffmpeg.probe(first)['streams'][0]
     f = ffmpeg.input(first)
-    s = ffmpeg.input(second).filter('scale',width=info['width'], height=info['height'], force_original_aspect_ratio='decrease').filter('pad', info['width'], info['height'], '(ow-iw)/2','(oh-ih)/2')
+    s = ffmpeg.input(second).filter('scale',
+                                    width=info['width'], 
+                                    height=info['height'], 
+                                    force_original_aspect_ratio='decrease')
+                                    .filter('pad', 
+                                            info['width'],
+                                            info['height'],
+                                            '(ow-iw)/2','(oh-ih)/2')
     ffmpeg.concat(f,s).output(endfile).overwrite_output().run()
 
     return endfile
---
+++ mew.py
@@ -0,0 +1,77 @@
+import discord
+from discord import File
+from discord.ext import commands
+from discord.ext.commands import Bot
+from discord.utils import get
+
+import os
+from os.path import join
+from os.path import dirname
+
+from dotenv import load_dotenv
+
+import commandutils
+import re
+import time
+import random
+import csv
+import pickle
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+BOT_PREFIX = ('!m ', '!mew ', '!Mew ', '!MEW ', '!M')
+
+description='''Mew - Python-based discord bot!'''
+bot = commands.Bot(command_prefix = BOT_PREFIX, description=description)
+
+@bot.commad(name='hi', description='Project description for Mew', aliases=['hello', 'hey', 'hallo'], pass_context=True)
+async def hi(context):
+    msg = '''Hi {}, I am Mew, a discord bot project by KeyBee#0811.
+    Github: https://github.com/BaardsethK/mew-bot
+    Trello: https://trello.com/b/9RsvmogR/mew-discord-bot'''.format(str(context.message.author))
+    await context.send(msg)
+
+@bot.command(name='gamestat', pass_context=True)
+async def gamestat(context, game, alias):
+    msg = commandutils.get_game_stats(game, alias)
+    await context.send(msg)
+
+@bot.command(name='uwuize', aliases=['uwu'], pass_context=True)
+async def uwuize(context, *, message):
+    msg = message.lower()
+    msg = msg.replace('pos', 'paws')
+    msg = msg.replace('r', 'w')
+    msg = msg.replace('l', 'w')
+    msg = msg.replace('th', 'd')
+    await context.send(msg)
+
+@bot.event
+async def on_message(message):
+    if message.author.bot == False:
+        channel = message.channel
+        server_id = str(message.guild.id)
+        if '/r/' in message.content:
+            subreddit = re.search(r'\/r\/((.*?)[^\s-.-,]+|[^\/]+)', message.content)
+            print(subreddit.group(0))
+            if len(subreddit.group(1)) > 20:
+                msg = comut.get_message_line('dismissive', moods[server_id])
+                await channel.send(msg)
+                await bot.process_commands(message)
+            elif subreddit:
+                msg = 'https://www.reddit.com{}'.format(subreddit.group(0))
+                await channel.send(msg)
+                await bot.process_commands(message)
+        else:
+            await bot.process_commands(message)
+
+@bot.event
+async def on_reay():
+    await bot.change_presence(activity=discord.Game(name=''))
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+bor.run(TOKEN)
